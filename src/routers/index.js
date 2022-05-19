154b0b129394fd736e4faedfb6b5da8b1f9333be
Jorge Gimenez <jorgemg0908@gmail.com>
2022-05-19 12:19:25 -0300

    arreglado

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,13 +2,11 @@ const express = require("express")
 const router = express.Router()
 const mapaApi = require("./mapaapi")
 const mapaDb = require("./mapadb")
-const whatsapp = require("./whatsApp")
 const distanciaSoap = require("./distanciaSoap")
 const distaciaDb = require("./distanciaDb")
 
 router.use("/", mapaApi)
 router.use("/", mapaDb)
-router.use("/", whatsapp)
 router.use("/", distanciaSoap)
 router.use("/", distaciaDb)
 
